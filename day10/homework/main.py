f486188a000e4315c2474575554719f24f5c5e71
andriagika <ultrasatesto@gmail.com>
2025-05-17 13:58:48 +0400

    commit

---
+++ day10/homework/main.py
@@ -0,0 +1,41 @@
+# type() ფუნქციის დანიშნულებაა რომ დაადგინოს ობიექტის ტიპი და დააბრუნოს გადაცემული ობიექტის ტიპი
+
+# str(), int(), float(), and bool()
+
+#str()
+number = 6
+number = str(number)
+print(type(number))
+
+# #int()
+num4 = int(input("Enter a number: "))
+num6 = int(input("Enter a number: "))
+print(num4 - num6)
+
+# #float()
+num = 26
+print(float(num))
+print(float(True))
+
+#bool() 
+print(bool(1))
+print(bool(0))
+
+int = str
+
+user_name =  int(input("Enter your name: "))
+user_surname = int(input("Enter your surname: "))
+user_age = int(input("Enter your age: "))
+
+print(int(f"The users name is {user_name} her/his age is {user_age} and her/his surname is {user_surname}"))
+
+
+num1 = int(input("Enter random number: "))
+num2 = int(input("Enter random number: "))
+num3 = int(input("Enter random number: "))
+num4 = int(input("Enter random number: "))
+num5 = int(input("Enter random number: "))
+num6 = 5
+print(f"The Aritmetical mean is {num1 + num2 + num3 + num4 + num5 } / {num6} ")
+
+
