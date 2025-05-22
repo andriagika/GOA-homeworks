06d3c0ac7128ed91c014f1805795fa8e17a1b895
andriagika <ultrasatesto@gmail.com>
2025-05-22 17:11:25 +0400

    commit

---
+++ day12/homework/main.py
@@ -0,0 +1,78 @@
+# ჩვენ ვისწავლეთ 6 შედარების ოპერატორი ესენია : მეტობის > , ნაკლებობის < , მეტია ან ტოლია >= , ნაკლებია ან ტოლია <= , უდრის უდრის == , არ უდრის != 
+
+lop = 12
+olp = 44
+print(lop > olp)
+
+u = 15
+p = 25
+print(u > p)
+
+h = 36
+t = 99
+print(t > h)
+
+f = 45
+g = 52
+print(f < g)
+
+c = 65
+q = 49
+print(q < c)
+
+vbn = 789
+wert = 890
+print(vbn < wert)
+
+
+b = 61
+a = 187
+print(a >= b)
+
+d = 33
+w = 33
+print(d >= w)
+
+zx = 123
+xc = 234
+print(zx >= xc)
+
+ad = 73
+ac = 55
+print(ac <= ad)
+
+we = 34
+dg = 34
+print (dg <= we)
+
+pdf = 445
+pol = 758
+print (pdf <= pol)
+
+fg = 669
+hg = 434
+print(fg == hg)
+
+qw = 5677
+jk = 5677
+print(qw == jk)
+
+qwer = 5344
+tyui = 5322
+print(qwer == tyui)
+
+mn = 53
+nm = 46
+print(nm != mn)
+
+bn = 12
+nb = 12
+print(bn != nb)
+
+nbb = 657
+mbb = 656
+print(nbb != mbb)
+
+
+
+ 
---
+++ day12/homework/main2.py
@@ -0,0 +1,23 @@
+my_height = 163
+user_heigh = int(input("Enter your height : "))
+if user_heigh > my_height :
+    print("you are taller than me.")
+if user_heigh < my_height :
+    print("you are shorter than me")
+if user_heigh == my_height :
+    print("we are same height")
+
+
+# num1 = "14"
+# num2 = 14
+# print(num1 == num2)
+
+# ეს მაგალითი False  იმიტომ გამოიტანს რომ ინტეგერი ვერ იქნება სტრინგის ტოლი
+
+
+my_surname = "Gikashvili"
+user_surname= (input("Enter your surname : "))
+if user_surname == my_surname :
+    print("we have same surname.")
+if user_surname != my_surname :
+    print("we dont have same surname.")
