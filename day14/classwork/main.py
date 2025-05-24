d0cb426189fde99cdfc27cea2da5d7b7bc8a9987
andriagika <ultrasatesto@gmail.com>
2025-05-24 20:17:20 +0400

    commit

---
+++ day14/classwork/main.py
@@ -0,0 +1,32 @@
+# logikuri operatorebi
+# 1) and ოპერატორი
+# 2) or ოპერატორი 
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
+
+# print(True and False or False and False or True)
+# # ეს გამოიტანს True-ს
+
+soil_moisture = input("Enter the temperature : ")
+print(soil_moisture > 100)
+
+print(6 > 2 and 9 > 3)
+print(8 == 8 and 5 < 2)
+print(4 != 4 and 10 > 2)
+print(4 < 1 and 10 > 20)
+
+print(2 > 3 or 3 > 2)
+print(4 < 5 or 4 > 5)
+print(6 > 12 or 5 == 5)
+print( 2 != 2 or 3 == 7)
+
