ed17cf647b4e90f932082d1a83de7d8114437ad0
andriagika <ultrasatesto@gmail.com>
2025-06-10 19:23:14 +0400

    commit

---
+++ day20/homework/main.py
@@ -0,0 +1,19 @@
+# name = input("Enter your surname: ")
+#     for i in name:
+#     print(i)
+
+# drinks = 300
+# while drinks >= 0:
+#     print("Drinks has been sold")
+#     drinks -= 2
+# print("All drinks have been sold")
+
+# for i in range(20,50,3):
+#     print(i)
+
+# i = 150
+# while i >= 1:
+#     i = i - 1
+#     print(i)
+
+# while loop Python-ში იმეორებს ბლოკს მანამდე, სანამ პირობა მართალია ანუ True
